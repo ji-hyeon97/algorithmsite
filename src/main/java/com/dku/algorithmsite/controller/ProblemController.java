81e691ea917dc89e9c201ea7a133b9eb41b44f78
ghdcksgml1 <ghdcksgml2@naver.com>
2022-05-04 09:45:41 +0900

    홍찬희) jsp=> thymeleaf

--- src/main/java/com/dku/algorithmsite/controller/ProblemController.java
+++ src/main/java/com/dku/algorithmsite/controller/ProblemController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProblemController {
 
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+
     @GetMapping("/hello")
     public String indexPage(){
         return "hello";
