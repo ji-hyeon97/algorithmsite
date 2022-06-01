5f0645222f5d9b9969a33c5c11cc04c0776def37
ghdcksgml1 <ghdcksgml2@naver.com>
2022-06-01 21:02:59 +0900

    홍찬희) 에러페이지 설정 (모든 에러코드 통합)

--- src/main/java/com/dku/algorithmsite/handler/GlobalExceptionHandler.java
+++ src/main/java/com/dku/algorithmsite/handler/GlobalExceptionHandler.java
@@ -1,14 +1,35 @@
 package com.dku.algorithmsite.handler;
 
+import com.dku.algorithmsite.config.auth.PrincipalDetail;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-    @ExceptionHandler(Exception.class)
-    public String error(){
-        return "error/error";
+@Slf4j
+@Controller
+public class GlobalExceptionHandler implements ErrorController {
+
+    @GetMapping("/errorPage")
+    public String handleError(HttpServletRequest request,Model model, @AuthenticationPrincipal PrincipalDetail principal) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        log.warn("Error code : {}",status);
+
+        model.addAttribute("status",status);
+        model.addAttribute("principal",principal);
+        if(principal != null) {
+            model.addAttribute("boj_username", principal.getBoj_username());
+        }
+
+        return "error/errorPage";
     }
 }
